f0783b53edeeed3a4b923ac9d2a7988c445051b9
Alejandra Arias <Alejandra.Arias@ilumno.com>
2024-07-09 11:38:40 -0500

    modificacion del index

--- index.js
+++ index.js
@@ -7,65 +7,76 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-bfc27286-d994-48ff-b197-04e636341953';  // Reemplaza con tu token de acceso privado
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+// Ruta GET para la página de inicio ("/")
+app.get('/', async (req, res) => {
+    const customObjectsUrl = 'https://api.hubapi.com/crm/v3/objects/contacts?properties=email,firstname,lastname,city'; // Reemplaza con el endpoint para contactos y propiedades específicas
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    }
+    };
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(customObjectsUrl, { headers });
+        const data = response.data.results;
+        res.render('home', { title: 'Home | HubSpot APIs', data });  // Usa la plantilla home.pug
     } catch (error) {
         console.error(error);
+        res.status(500).send('Error retrieving custom object data');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// Ruta GET para el formulario HTML ("/update-cobj")
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Formulario de actualización de objeto personalizado | Integración con HubSpot I Práctica' });
+});
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+// Ruta POST para enviar los datos capturados por el formulario HTML ("/update-cobj")
+app.post('/update-cobj', async (req, res) => {
+    const customObjectData = {
+        properties: {
+            email: req.body.email,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+            city: req.body.city
+        }
+    };
+
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    // Buscar el contacto por email para obtener su ID
+    const searchUrl = `https://api.hubapi.com/crm/v3/objects/contacts/search`;
+    const searchData = {
+        filterGroups: [{
+            filters: [{
+                propertyName: 'email',
+                operator: 'EQ',
+                value: req.body.email
+            }]
+        }]
+    };
+
+    try {
+        const searchResponse = await axios.post(searchUrl, searchData, { headers });
+        if (searchResponse.data.results.length > 0) {
+            // Contacto existente encontrado, actualizar
+            const contactId = searchResponse.data.results[0].id;
+            const updateUrl = `https://api.hubapi.com/crm/v3/objects/contacts/${contactId}`;
+            await axios.patch(updateUrl, customObjectData, { headers });
+        } else {
+            // No se encontró contacto, crear uno nuevo
+            const createUrl = 'https://api.hubapi.com/crm/v3/objects/contacts';
+            await axios.post(createUrl, customObjectData, { headers });
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error("Error details:", error.response ? error.response.data : error.message);
+        res.status(500).send('Error creating or updating custom object data');
     }
-
 });
-*/
 
-
-// * Localhost
+// Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
